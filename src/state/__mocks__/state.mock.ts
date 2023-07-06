f68f676e33abcc7856240eebc352e403558013de
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-07-06 19:19:30 -0300

    feat: create unit test in state and city Entities

---
+++ src/state/__mocks__/state.mock.ts
@@ -0,0 +1,9 @@
+import { StateEntity } from '../entities/state.entity';
+
+export const stateMock: StateEntity = {
+  createdAt: new Date(),
+  id: 4845730,
+  name: 'stateNameMock',
+  uf: 'MK',
+  updatedAt: new Date(),
+};
