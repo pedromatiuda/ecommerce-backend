689d449d4515b387d463309116fa7c42208a4a6f
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-09-19 17:35:58 -0300

    feat: create categoryService

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReturnCategory } from './dtos/returnCategory.dto';
+import { CategoryService } from './category.service';
+import { UserType } from 'src/user/enum/user-type.enum';
+import { Roles } from 'src/decorators/roles.decorator';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async findAllCategories(): Promise<ReturnCategory[]> {
+    return (await this.categoryService.findAllCategories()).map(
+      (category) => new ReturnCategory(category),
+    );
+  }
+}
